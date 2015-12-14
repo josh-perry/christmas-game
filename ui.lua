b2cc31b2a026bab43918818c47d141dd25fd8e40
Josh Perry <joshperry@outlook.com>
2015-12-14 20:39:37 +0000

    Removed dumb debug thing

--- ui.lua
+++ ui.lua
@@ -12,29 +12,24 @@ function UI:initialize()
 end
 
 function UI:draw()
-    -- love.graphics.setCanvas(self.ui_canvas)
-    --
+    -- Rotate around the center of the screen
     love.graphics.setColor(255, 255, 255, 10)
     love.graphics.translate(screen_width/2, screen_height/2)
     love.graphics.rotate(self.r)
     love.graphics.translate(-screen_width/2, -screen_height/2)
+
     love.graphics.setFont(self.font)
     love.graphics.printf(self.present_text.text, 0, 10, screen_width, "center")
+
     love.graphics.rotate(0)
     love.graphics.translate(0, 0)
-    --
-    -- love.graphics.setCanvas()
-    --
-    -- love.graphics.draw(self.ui_canvas, 0, 0, self.r)
-    --
-    -- self.ui_canvas:clear()
 end
 
 function UI:update(dt)
 end
 
 function UI:update_presents_ui(delivered)
-    if not delivered then delivered = math.random(0, 10) end
+    if not delivered then delivered = 0 end
 
     self.present_text = {
         sx = math.random(1, 2),
